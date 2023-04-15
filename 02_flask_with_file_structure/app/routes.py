85a58a69dabe3dbf75c69562208bae8e1bcca6db
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-04-15 09:58:01 -0700

    Update routes.py

--- 02_flask_with_file_structure/app/routes.py
+++ 02_flask_with_file_structure/app/routes.py
@@ -41,3 +41,30 @@ def login():
 @myapp_obj.route("/members/<string:name>/")
 def getMember(name):
     return escape(name)
+
+
+@myapp_obj.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        phoneNumber = request.form['phone number']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', phoneNumber):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
