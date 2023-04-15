213d68044bb87fb09e4f686a6a47867db7c60ac5
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-04-15 09:05:42 -0700

    update routes

--- 02_flask_with_file_structure/app/routes.py
+++ 02_flask_with_file_structure/app/routes.py
@@ -1,19 +1,72 @@
-from app import myapp_obj
-from flask import escape
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+ 
+@PioRomo 
+ProfRojas
+/
+flask
+Public
+Fork your own copy of ProfRojas/flask
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+flask/10_login/app/routes.py /
+@carlosrojas
+carlosrojas Add database and login files
+Latest commit ae2b548 on Mar 13
+ History
+ 1 contributor
+43 lines (39 sloc)  1.24 KB
+ 
 
+from flask import render_template
+from flask import redirect
+from flask import flash
+from .forms import LoginForm
+from app import myapp_obj
+from flask_login import current_user
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import login_required
 
 @myapp_obj.route("/")
 @myapp_obj.route("/index.html")
 def index():
-    return "hi"
+    name = 'Carlos'
+    books = [ {'author': 'authorname1',
+                'book':'bookname1'},
+             {'author': 'authorname2',
+              'book': 'bookname2'}]
+    return render_template('hello.html',name=name, books=books)
 
 @myapp_obj.route("/hello")
+@login_required
 def hello():
     return "Hello World!"
 
-@myapp_obj.route("/login")
+@myapp_obj.route("/login", methods=['GET', 'POST'])
 def login():
-    return "Login Page!"
+    # create form
+    form = LoginForm()
+    # if form inputs are valid
+    if form.validate_on_submit():
+        # search database for username
+        # user = User.query.filter_by(...)
+        # check the password
+        # if password matches
+        # login_user(user)
+        flash(f'Here are the input {form.username.data} and {form.password.data}')
+        return redirect('/')
+    return render_template('login.html', form=form)
 
 @myapp_obj.route("/members/<string:name>/")
 def getMember(name):
