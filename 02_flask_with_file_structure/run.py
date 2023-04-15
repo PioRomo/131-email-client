e2756d89f716f9e426436f092620ec3477563c4a
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-04-15 08:57:27 -0700

    Update run.py

--- 02_flask_with_file_structure/run.py
+++ 02_flask_with_file_structure/run.py
@@ -1,19 +1,2 @@
 from app import myapp_obj
-from flask import escape
-
-@myapp_obj.route("/")
-@myapp_obj.route("/index.html")
-def index():
-    return "hi"
-
-@myapp_obj.route("/hello")
-def hello():
-    return "Hello World!"
-
-@myapp_obj.route("/login")
-def login():
-    return "Login Page!"
-
-@myapp_obj.route("/members/<string:name>/")
-def getMember(name):
-    return escape(name)
+myapp_obj.run()
