b21d77c749ef87d46113e1e14440144193be3f1e
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-04-17 13:20:48 -0700

    Update __init__.py

--- 02_flask_with_file_structure/app/__init__.py
+++ 02_flask_with_file_structure/app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 myapp_obj = Flask(__name__)
 
 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 myapp_obj.config.from_mapping(
@@ -14,10 +15,15 @@ myapp_obj.config.from_mapping(
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 )
 
+
 db = SQLAlchemy(myapp_obj)
+
+db.init_app(myapp_obj)
 
 login = LoginManager(myapp_obj)
 
 login.login_view = 'login'
 
 from app import routes, models
+
+return app
