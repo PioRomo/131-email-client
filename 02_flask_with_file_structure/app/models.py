ab87bd0b28d2220dd45893cc0c0c1ba3def5a1b1
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-04-20 10:39:07 -0700

    Update models.py

--- 02_flask_with_file_structure/app/models.py
+++ 02_flask_with_file_structure/app/models.py
@@ -7,7 +7,7 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), nullable=False)
-    password = db.Column(db.String(32), nullable=False)
+    password = db.Column(db.String, nullable=False)
     phonenumber = db.Column(db.String(100), nullable=False)
 
     posts = db.relationship('Post', backref='author', lazy='dynamic')
