461ab42664dae17fe22e0aff493aba9a457e7673
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-04-20 10:47:25 -0700

    Update routes.py

--- 02_flask_with_file_structure/app/routes.py
+++ 02_flask_with_file_structure/app/routes.py
@@ -78,7 +78,7 @@ def register():
             flash('Phone number invalid!')
             return redirect(url_for('register'))
             
-        new_user = User(phonenumber=phonenumber, username=username, password=password)
+        new_user = User(phonenumber=phonenumber, username=username, password=generate_password_hash(password, method='sha256'))
        #new_user.set_password(new_user.password)
         db.session.add(new_user)
         db.session.commit()
