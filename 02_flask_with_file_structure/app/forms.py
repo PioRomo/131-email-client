9c2e643b4777b8e3139b45d3537c3ff50bc00e0e
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-04-20 08:46:52 -0700

    Update forms.py

--- 02_flask_with_file_structure/app/forms.py
+++ 02_flask_with_file_structure/app/forms.py
@@ -3,7 +3,12 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Userename', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    phonenumber = StringField('Phonenumber', validators=[DataRequired()])
