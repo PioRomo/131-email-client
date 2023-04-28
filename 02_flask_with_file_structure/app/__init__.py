d9a03f5e0ee1042d7020d31f8d8f9af8d1ff946e
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-04-28 08:34:23 -0700

    Update __init__.py

--- 02_flask_with_file_structure/app/__init__.py
+++ 02_flask_with_file_structure/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 import os
 from flask_login import LoginManager
 
