c617b721d143ac5a401d123575da525f3caa1fee
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-04-28 08:34:50 -0700

    Update forms.py

--- 02_flask_with_file_structure/app/forms.py
+++ 02_flask_with_file_structure/app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
