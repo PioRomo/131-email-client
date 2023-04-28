3b00085746c5d8ecff2695ff5ac03be645d6319d
Pio L Romo <104649165+PioRomo@users.noreply.github.com>
2023-04-28 08:44:04 -0700

    Update __init__.py

--- 02_flask_with_file_structure/app/__init__.py
+++ 02_flask_with_file_structure/app/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 myapp_obj = Flask(__name__)
 
 photos = UploadSet('photos', IMAGES)
-configure_uploads(app, photos)
+configure_uploads(myapp_obj, photos)
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
