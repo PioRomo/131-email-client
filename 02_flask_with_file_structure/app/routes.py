c8515ffafbaf85470f3d79485435d03ea07af58a
thienbryannguyen6670 <125083690+thienbryannguyen6670@users.noreply.github.com>
2023-05-14 20:24:20 -0700

    Update routes.py

--- 02_flask_with_file_structure/app/routes.py
+++ 02_flask_with_file_structure/app/routes.py
@@ -300,6 +300,18 @@ def chat():
                 new_msg=Chat(searched_for = False, recipient = recipient, msg = msg)
                 sender = current_user.username
                 sender = "Me"
-
+                new_email.sender=sender
+                users = User.query.all()
+                uid = "null"
+                #find which user the email is being sent to
+                for i in users:
+                        if i.username == recipient:
+                                uid = i.id
+                new_email.user_id = uid
+                #if user does not exist, tell user to try again
+                if uid == "null":
+                        flash("The user you entered does not exist")
+                db.session.add(new_chat)
+                db.session.commit()
         return redirect(url_for('chat'))
         return render_template('chat.html')
